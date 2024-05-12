d6187866ac76fd3a475c88c652694a1e0544892f
YonyVilca <yvilcam@ulasalle.edu.pe>
2024-05-11 20:40:59 -0500

    se implemento la interfaz de welcome_fragment

--- app/src/main/java/com/example/welcomefragment/WelcomeFragment.kt
+++ app/src/main/java/com/example/welcomefragment/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class WelcomeFragment : Fragment() {
 
@@ -18,7 +20,17 @@ class WelcomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val root = inflater.inflate(R.layout.fragment_welcome, container, false)
+
+        val buttonNavigate = root.findViewById<Button>(R.id.buttonNavigate)
+        buttonNavigate.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_questionFragment)
+        }
+
+
+        return root
+
+
     }
 
 
